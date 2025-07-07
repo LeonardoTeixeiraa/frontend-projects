659528eb35eed3542b1e6cd8d2a6064258a970cc
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2025-07-07 12:09:32 -0300

    feat: Implementa um exemplo de exercício que calcula e mostra  o salário final de um colaborador.

---
+++ JavaScript-Learning/JavaScript_Introduction/calcularSalario/script.js
@@ -0,0 +1,9 @@
+//Faça um programa que receba o salário base de um funcionário.
+//Calcule e mostre o salário a receber, sabendo que esse funcionário tem gratificação
+//de R$ 50,00 e paga imposto de 10% sobre o salário base
+
+let salarioBase = 1000.00;
+
+let salarioFinal = (salarioBase * 1.1) + 50.00;
+
+console.log("O salário final é: " + salarioFinal);
