77dd9602add707a35a621c4da06cf3682854661f
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2025-07-07 20:09:41 -0300

    feat: Implementa um exercício sobre tratamento de exceções em Javascript com a estrutura try-catch e erroscom mensagens personalizadas com o throw new

---
+++ JavaScript-Learning/JavaScript_Introduction/debug-error/script.js
@@ -0,0 +1,32 @@
+function validaArray(arr, num) {
+  try {
+    if (!arr || !num) throw new ReferenceError("Envie os parâmetros");
+
+    if (typeof arr !== 'object')
+      throw new TypeError("O Array precisa ser do tipo Object");
+
+    if (typeof num !== 'number')
+      throw new TypeError("O Array precisa ser do tipo number");
+
+    if (arr.length !== num) throw new RangeError("Tamanho inválido, informe um numero correspondente ao comprimento do array");
+
+    return arr;
+  } catch (e) {
+    if (e instanceof ReferenceError) {
+      console.log("Este é um erro de ReferenceError");
+      console.log(e.message);
+    } else if (e instanceof TypeError) {
+      console.log("Este é um erro de TypeError");
+      console.log(e.message);
+    } else if (e instanceof RangeError) {
+      console.log("Este é um erro de RangeError");
+      console.log(e.message);
+    } else {
+      console.log("Tipo de erro não esperado: " + e);
+    }
+  }
+}
+
+console.log(validaArray([],2));
+console.log(validaArray([1,2], 5));
+console.log(validaArray([1,2,3,4,5], 5));
